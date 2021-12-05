3ca2de491ccdeea7c85a7cce07be289ba9bbae9d
eryn L. K <eryn@eryn.io>
2021-12-05 04:44:53 -0500

    Add throttle to color change

--- example/server/systems/partsChangeColor.lua
+++ example/server/systems/partsChangeColor.lua
@@ -7,19 +7,24 @@ local useDeltaTime = Matter.useDeltaTime
 local BoundInstance = Components.BoundInstance
 
 local function partsChangeColor(world)
-	for entityId, boundInstance in world:query(BoundInstance) do
+	for entityId, boundInstance in world:query(BoundInstance):without(Components.ColorTween) do
 		useEvent(boundInstance.instance.Touched, function()
 			world:insert(
 				entityId,
 				Components.ColorTween({
 					goal = BrickColor.random().Color,
+					time = os.clock(),
 				})
 			)
 		end)
 	end
 
-	for _entityId, boundInstance, tween in world:query(BoundInstance, Components.ColorTween) do
-		boundInstance.instance.Color = boundInstance.instance.Color:lerp(tween.goal, useDeltaTime())
+	for entityId, boundInstance, tween in world:query(BoundInstance, Components.ColorTween) do
+		boundInstance.instance.Color = boundInstance.instance.Color:lerp(tween.goal, useDeltaTime() * 2)
+
+		if os.clock() - tween.time > 1 then
+			world:remove(entityId, Components.ColorTween)
+		end
 	end
 end
 

cf2d5687d6ad75ef96ceab0afb07cba4a6aba959
eryn L. K <eryn@eryn.io>
2021-12-20 00:15:40 -0500

    Re-export Llama.Dictionary.merge

--- lib/init.lua
+++ lib/init.lua
@@ -1,5 +1,6 @@
 -- local t = require(script.Parent.t)
 
+local Llama = require(script.Parent.Llama)
 local World = require(script.World)
 local Loop = require(script.Loop)
 local newComponent = require(script.Component).newComponent
@@ -13,4 +14,6 @@ return {
 	useEvent = require(script.hooks.useEvent),
 	useDeltaTime = require(script.hooks.useDeltaTime),
 	useThrottle = require(script.hooks.useThrottle),
+
+	merge = Llama.Dictionary.merge,
 }

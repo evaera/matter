e805b46efd9ac53606dfc4d5bda0e8789d024bf8
eryn L. K <eryn@eryn.io>
2021-12-27 04:22:56 -0500

    Add new branding

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <div align="center">
-	<h1>Matter</h1>
+	<img src=".moonwave/static/logo.svg" alt="Matter" />
 	<p>A modern ECS library for Roblox</p>
-	<a href="https://eryn.io/matter"><strong>View docs</strong></a>
+	<a href="https://eryn.io/matter"><strong>View docs &rarr;</strong></a>
   <br><br><br>
 
   Made by [@lpghatguy](https://github.com/lpghatguy) and [@evaera](https://github.com/evaera) ❤️
---
+++ pages/index.js
@@ -0,0 +1,92 @@
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import React from "react";
+import "./home.css";
+import styles from "./index.module.css";
+
+const FEATURES = [];
+
+function Feature({ image, title, description }) {
+  return (
+    <div className={clsx("col col--4")}>
+      {image && (
+        <div className="text--center">
+          <img className={styles.featureSvg} alt={title} src={image} />
+        </div>
+      )}
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export function HomepageFeatures() {
+  if (!FEATURES) return null;
+
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FEATURES.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">
+          <img
+            src={siteConfig.baseUrl + "logo.svg"}
+            className="bigLogo"
+            alt="Moonwave"
+          />
+        </h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Get Started →
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig, tagline } = useDocusaurusContext();
+  return (
+    <Layout title={siteConfig.title} description={tagline}>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+        <p
+          className="container"
+          style={{
+            maxWidth: "800px",
+            fontSize: "22px",
+            padding: "0 30px",
+          }}
+        >
+          Matter is a pure ECS library that empowers users to build games that
+          are extensible, performant, and easy to debug, featuring fast
+          archetypical entity storage, automatic system scheduling, and
+          topologically-aware APIs.
+        </p>
+      </main>
+    </Layout>
+  );
+}

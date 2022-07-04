7aef20a854414eb2ed318a34a100cdce0aa136c5
finobinos <muhammadahmednasir3000@gmail.com>
2022-07-05 03:18:54 +0500

    Commit

--- lib/hooks/useEvent.lua
+++ lib/hooks/useEvent.lua
@@ -1,47 +1,72 @@
 local topoRuntime = require(script.Parent.Parent.topoRuntime)
 local Queue = require(script.Parent.Parent.Queue)
 
-local EVENT_CONNECT_METHODS = {"Connect", "on", "connect"}
-local CONNECTION_DISCONNECT_METHODS = {"Disconnect", "disconnect", "Destroy", "destroy"}
+local EVENT_CONNECT_METHODS = { "Connect", "on", "connect" }
+local CONNECTION_DISCONNECT_METHODS = { "Disconnect", "disconnect", "Destroy", "destroy" }
 
-local function connect(event, callback)
-	-- Get the 'Connect' method through duck typing, since sometimes developers
-	-- may have custom events that they want to connect through this function:
-	local connectMethod 
+local function connect(object, callback, event)
+	if typeof(event) == "RBXScriptSignal" then
+		return event:Connect(callback)
+	end
 
-	if type(event) == "table" then
-		for _, method in EVENT_CONNECT_METHODS do
-			if type(event[method]) == "function" then
-				connectMethod = method
-				break
-			end
-		end							
+	if type(event) == "string" then
+		object = object[event]
 	end
-									
-	if connectMethod == nil then
-		error("Couldn't connect to event as no valid connect methods were found! Ensure the passed event has a 'Connect' or an 'on' method!")						
+
+	if type(object) == "function" then
+		return object()
+	elseif typeof(object) == "RBXScriptSignal" then
+		return object:Connect(callback)
 	end
-	
-	return event[connectMethod](event, callback)
+
+	for _, method in EVENT_CONNECT_METHODS do
+		if type(object) ~= "table" or type(object[method]) ~= "function" then
+			continue
+		end
+
+		return object[method](object, callback)
+	end
+
+	error(
+		"Couldn't connect to event as no valid connect methods were found! Ensure the passed event has a 'Connect' or an 'on' method!"
+	)
 end
 
 local function disconnect(connection)
-	-- Get a 'Disconnect' method through duck typing, since sometimes developers
-	-- may have custom events which may return custom connection objects..
-	local disconnectMethod 
+	if typeof(connection) == "function" then
+		connection()
+		return
+	end
 
 	for _, method in CONNECTION_DISCONNECT_METHODS do
-		if type(connection[method]) == "function" then
-			disconnectMethod = method
-			break
+		if type(connection[method]) ~= "function" then
+			continue
 		end
-	end							
-									
-	if disconnectMethod == nil then
-		error("Couldn't disconnect event as no valid disconnect methods were found! Ensure the passed event returns a connection object with a 'Disconnect' or a 'Destroy' method at least!")						
+
+		connection[method](connection)
+		break
 	end
-	
-	connection[disconnectMethod](connection)
+end
+
+local function validateConnection(connection)
+	if typeof(connection) == "function" or typeof(connection) == "RBXScriptConnection" then
+		return
+	end
+
+	local disconnectMethod
+
+	for _, method in CONNECTION_DISCONNECT_METHODS do
+		if type(connection) ~= "table" or connection[method] == nil then
+			continue
+		end
+
+		disconnectMethod = method
+		break
+	end
+
+	if disconnectMethod == nil then
+		error("Ensure passed event returns a connection object with a 'Disconnect' or a 'Destroy' method!")
+	end
 end
 
 local function cleanup(storage)
@@ -51,13 +76,10 @@ end
 
 --[=[
 	@within Matter
-
 	:::info Topologically-aware function
 	This function is only usable if called within the context of [`Loop:begin`](/api/Loop#begin).
 	:::
-
 	Collects events that fire during the frame and allows iteration over event arguments.
-
 	```lua
 	for _, player in ipairs(Players:GetPlayers()) do
 		for i, character in useEvent(player, "CharacterAdded") do
@@ -70,47 +92,34 @@ end
 		end
 	end
 	```
-
 	Returns an iterator function that returns an ever-increasing number, starting at 1, followed by any event arguments
 	from the specified event.
-
 	Events are returned in the order that they were fired.
-
 	:::caution
 	`useEvent` keys storage uniquely identified by **the script and line number** `useEvent` was called from, and the
 	first parameter (instance). If the second parameter, `event`, is not equal to the event passed in for this unique
 	storage last frame, the old event is disconnected from and the new one is connected in its place.
-
 	Tl;dr: on a given line, you should hard-code a single event to connect to. Do not dynamically change the event with
 	a variable. Dynamically changing the first parameter (instance) is fine.
-
 	```lua
 	for _, instance in pairs(someTable) do
 		for i, arg1, arg2 in useEvent(instance, "Touched") do -- This is ok
-
 		end
 	end
-
 	for _, instance in pairs(someTable) do
 		local event = getEventSomehow()
 		for i, arg1, arg2 in useEvent(instance, event) do -- PANIC! This is NOT OK
-
 		end
 	end
 	```
 	:::
-
 	If `useEvent` ceases to be called on the same line with the same instance and event, the event connection is
 	disconnected from automatically.
-
 	You can also pass the actual event object instead of its name as the second parameter:
-
 	```lua
 	useEvent(instance, instance.Touched)
-
 	useEvent(instance, instance:GetPropertyChangedSignal("Name"))
 	```
-
 	@param instance Instance -- The instance that has the event you want to connect to
 	@param event string | RBXScriptSignal -- The name of or actual event that you want to connect to
 ]=]
@@ -119,10 +128,6 @@ local function useEvent(instance, event): () -> (number, ...any)
 	assert(event ~= nil, "Event is nil")
 
 	local storage = topoRuntime.useHookState(instance, cleanup)
-
-	if type(event) == "string" then
-		event = instance[event]
-	end
 
 	if storage.event ~= event then
 		if storage.event then
@@ -134,11 +139,12 @@ local function useEvent(instance, event): () -> (number, ...any)
 		local queue = Queue.new()
 		storage.queue = queue
 		storage.event = event
-									
-		local connection = connect(event, function(...)
-			queue:pushBack(table.pack(...))
-		end)
 
+		local connection = connect(instance, function(...)
+			queue:pushBack(table.pack(...))
+		end, event)
+
+		validateConnection(connection)
 		storage.connection = connection
 	end
 

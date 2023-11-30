993e3b7e1ca782ff50673e8411954d80f620c72e
Jack T <44332148+jackTabsCode@users.noreply.github.com>
2023-11-30 03:57:18 -0800

    check if the world contains an entity before displaying the tooltip

--- lib/debugger/widgets/hoverInspect.lua
+++ lib/debugger/widgets/hoverInspect.lua
@@ -4,6 +4,8 @@ local FormatMode = formatTableModule.FormatMode
 
 return function(plasma)
 	return plasma.widget(function(world, id, custom)
+		if not world:contains(id) then return end
+			
 		local entityData = world:_getEntity(id)
 
 		local str = "Entity " .. id .. "\n\n"
